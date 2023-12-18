0dc34bf15bef726e32cca2fd93002de7c29e948c
Max <ppmax@bk.ru>
2023-12-18 23:45:32 +0300

    useQuery moved to it's hook

--- src/hooks/useTodosQuery.ts
+++ src/hooks/useTodosQuery.ts
@@ -1,9 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchTodos } from '../services/todos';
+import { TodoState } from '../types/todo';
 
-const useTodosQuery = () => useQuery({
-  queryFn: () => fetchTodos('all'),
-  queryKey: ['todos', 'all'],
+const useTodosQuery = (state: TodoState) => useQuery({
+  queryFn: () => fetchTodos(state),
+  queryKey: ['todos', state],
+  staleTime: 1000 * 5,
 });
 
 export default useTodosQuery;
