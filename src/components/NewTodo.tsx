06dc1363570be3494f5e0b70a979eb53b3a7f9c5
Max <ppmax@bk.ru>
2023-12-18 23:46:58 +0300

    New todo refresh optimization

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -2,14 +2,23 @@ import { Stack, Input, Button } from '@chakra-ui/react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useState } from 'react';
 import { createTodo } from '../services/todos';
+import { Todo } from '../types/todo';
 
 const NewTodo = () => {
   const [title, setTitle] = useState('');
   const client = useQueryClient();
   const { mutate: create } = useMutation({
     mutationFn: createTodo,
-    onSuccess: () => {
-      client.invalidateQueries({ queryKey: ['todos', 'all'] });
+    // onSuccess: () => {
+    //   client.invalidateQueries({ queryKey: ['todos', 'all'] });
+    // },
+    onSuccess: (newTodo) => {
+      // client.getQueryData(['todos', 'all']);
+      client.setQueryData<Todo[]>(['todos', 'all'], (oldTodos) => [...(oldTodos || []), newTodo]);
+      client.invalidateQueries({
+        queryKey: ['todos', 'all'],
+        refetchType: 'none',
+      });
     },
   });
 
